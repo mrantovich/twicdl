959465b4162bd631810463fb6274bc34d3a616a0
Mikhail Rantovich <mrantovich@gmail.com>
2018-08-09 15:10:41 +0700

    Add do_update()
    Implemented update and merge function. Striped unnecessary comments.

--- twicdl.py
+++ twicdl.py
@@ -51,41 +51,12 @@ group.add_argument("-m", "--merge", help="Download updates, merge them into one 
                     action="store_true")
 args = parser.parse_args()
 
-verbose_flag = True if args.verbose else False
-
 
 def form_twic_url(num):
     BASE_TWIC_URL = "http://www.theweekinchess.com/zips/twic"
     END_TWIC_URL = "g.zip"
     url = "".join((BASE_TWIC_URL, str(num), END_TWIC_URL))
     return url
-
-def check_updates(verbosity=True):
-    url_for_request = form_twic_url(NUMBER)
-    request_code = urllib.request.urlopen(url_for_request).getcode()
-    if request_code == 200:
-        if verbosity:
-            print("Some updates available!")
-    else:
-        if verbosity:
-            print("No updates detected.")
-    exit(0)
-
-def do_update(merge=False):
-    # Not implemented yet.
-    pass
-#     os.chdir(twdata_dir)
-#     data_content = os.listdir()
-#     if data_content:
-#         for exfile in data_content:
-#             if exfile.lower().endswith(".pgn")
-
-if args.check:
-    check_updates()
-elif args.update:
-    do_update()
-elif args.merge:
-    do_update(merge=True)
 
 def extract_pgn_files(path, verbosity=False):
     os.chdir(path)
@@ -101,48 +72,116 @@ def extract_pgn_files(path, verbosity=False):
                 print("Deleting zip-archive: %s..." % z)
             os.remove(z)
 
-
 def make_one_pgn(pgnpath):
     os.chdir(PGN_PATH)
+    # Check if TWIC.pgn file already exists and delete it.
+    if os.path.exists(pgnpath):
+        os.remove(pgnpath)
     pgnlist = os.listdir()
     with open(pgnpath, "wb") as bigpgn:
         for pgn in pgnlist:
             with open(pgn, "rb") as pgncontent:
                 shutil.copyfileobj(pgncontent, bigpgn, 1024*1024*10)
 
-def main(num, pgn_path, verbosity=False):
-    
+def check_updates(verbosity=True):
+    url_for_request = form_twic_url(NUMBER)
+    request_code = urllib.request.urlopen(url_for_request).getcode()
+    if request_code == 200:
+        if verbosity:
+            print("Some updates available!")
+    else:
+        if verbosity:
+            print("No updates detected.")
+    exit(0)
+
+def do_update(start_num, merge=False, verbosity=False):
+    num = start_num
+    os.chdir(twdata_dir)
+    data_content = os.listdir()
+#     if data_content:
+#         for exfile in data_content:
+#             base, ext = exfile.split(".")
+#             if ext.lower() == "pgn":
+#                 if NUMBER > base:
     while True:
-        url_for_download = form_twic_url(num)
-        filename = "".join(("twic",str(num),"g.zip"))
-        try:
+        num_filename = "".join(("twic", str(num), ".pgn"))
+        if num_filename in data_content:
             if verbosity:
-                print("Trying to download %s..." % filename)
-            if os.path.exists(os.path.join(pgn_path, filename)):
-                if verbosity:
-                    print("%s already exists. Skipped." % filename)
-            else:
-                response = urllib.request.urlopen(url_for_download)
-                data = response.read()
-                new_file = os.path.join(pgn_path,filename)
-                data_file = open(new_file, "wb")
-                data_file.write(data)
-                data_file.close()
-                if verbosity:
-                    print("%s saved." % filename)
+                print("%s already exists! Nothing to do." % num_filename)
             num += 1
-        except urllib.error.HTTPError:
-            # HTTP Error 404: file not found on the server.
-            # Here we must abort trying to download new files.
-            if verbosity:
-                print("No such file: %s" % filename)
-                print("All downloads finished. Exiting...")
-            break
-    extract_pgn_files(pgn_path, verbosity)
-    make_one_pgn("twic.pgn")
+        else:
+            url_for_download = form_twic_url(num)
+            filename = "".join(("twic",str(num),"g.zip"))
+            try:
+                if verbosity:
+                    print("Trying to download %s..." % filename)
+                if os.path.exists(os.path.join(PGN_PATH, filename)):
+                    if verbosity:
+                        print("%s already exists. Skipped." % filename)
+                else:
+                    response = urllib.request.urlopen(url_for_download)
+                    data = response.read()
+                    new_file = os.path.join(PGN_PATH,filename)
+                    data_file = open(new_file, "wb")
+                    data_file.write(data)
+                    data_file.close()
+                    if verbosity:
+                        print("%s saved." % filename)
+                num += 1
+            except urllib.error.HTTPError:
+                # HTTP Error 404: file not found on the server.
+                # Here we must abort trying to download new files.
+                if verbosity:
+                    print("No such file: %s" % filename)
+                    print("All downloads finished.")
+                break
+    extract_pgn_files(PGN_PATH, verbosity)
+    if merge:
+        if verbosity:
+            print("Merging files...")
+        make_one_pgn("TWIC.pgn")
+    if verbosity:
+        print("All done. Exiting...")
+                
+
+if args.check:
+    check_updates()
+elif args.update:
+    do_update(NUMBER, verbosity=args.verbose)
+elif args.merge:
+    do_update(NUMBER, merge=True, verbosity=args.verbose)
 
 
-if __name__ == "__main__":
-    main(NUMBER, PGN_PATH, verbose_flag)
+# def main(num, pgn_path, verbosity=False):
+#     
+#     while True:
+#         url_for_download = form_twic_url(num)
+#         filename = "".join(("twic",str(num),"g.zip"))
+#         try:
+#             if verbosity:
+#                 print("Trying to download %s..." % filename)
+#             if os.path.exists(os.path.join(pgn_path, filename)):
+#                 if verbosity:
+#                     print("%s already exists. Skipped." % filename)
+#             else:
+#                 response = urllib.request.urlopen(url_for_download)
+#                 data = response.read()
+#                 new_file = os.path.join(pgn_path,filename)
+#                 data_file = open(new_file, "wb")
+#                 data_file.write(data)
+#                 data_file.close()
+#                 if verbosity:
+#                     print("%s saved." % filename)
+#             num += 1
+#         except urllib.error.HTTPError:
+#             # HTTP Error 404: file not found on the server.
+#             # Here we must abort trying to download new files.
+#             if verbosity:
+#                 print("No such file: %s" % filename)
+#                 print("All downloads finished. Exiting...")
+#             break
+#     extract_pgn_files(pgn_path, verbosity)
+#     make_one_pgn("twic.pgn")
+
 
     
